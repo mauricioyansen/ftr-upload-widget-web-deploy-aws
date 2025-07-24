2ba252d7f7d22bbe817686e20e0447a48567f593
Mauricio <mauricio_cy@hotmail.com>
2025-07-24 20:52:03 -0300

    new: initial commit

---
+++ src/http/upload-file-to-storage.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+interface UploadFileToStorageParams {
+  file: File;
+  onProgress: (sizeInBytes: number) => void;
+}
+
+interface UploadFileToStorageOpts {
+  signal?: AbortSignal;
+}
+
+export async function uploadFileToStorage(
+  { file, onProgress }: UploadFileToStorageParams,
+  opts?: UploadFileToStorageOpts
+) {
+  const data = new FormData();
+
+  data.append("file", file);
+
+  const res = await axios.post<{ url: string }>(
+    "http://localhost:3333/uploads",
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      signal: opts?.signal,
+      onUploadProgress(progressEvent) {
+        onProgress(progressEvent.loaded);
+      },
+    }
+  );
+
+  return { url: res.data.url };
+}
